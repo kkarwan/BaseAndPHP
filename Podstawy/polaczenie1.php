8d00dae16aeae8a359a3787978581768b23928a7
Krzysztof Karwan <krzysztof.karwan@hotmail.com>
2023-12-19 17:56:07 +0100

    Dodanie do przykąłdu wyświetlania danych za pomcą pętli while

--- Podstawy/polaczenie1.php
+++ Podstawy/polaczenie1.php
@@ -6,7 +6,8 @@ $con = mysqli_connect("localhost", "root", "", "naukad");
 $query = "SELECT * FROM nazwiska";
 $result = mysqli_query($con,$query);
 
-// Kilka możliwości pokazania zawartości tabeli nazwiska z bazy naukad
+// Pokazania zawartości tabeli nazwiska z bazy naukad
+// dla wybranych pól za pomocą foreach
 
 // foreach
 
@@ -18,6 +19,21 @@ foreach ($result as $row) {
    echo ', ';
    echo '<a href="#">'.$row['miasto'].'</a>';
    echo '</p>';
+}
+
+// Pokazania zawartości tabeli nazwiska z bazy naukad
+// dla wybranych pól za pomocą while
+
+// while
+
+$result = mysqli_query($con,$query);
+
+while($row = mysqli_fetch_assoc($result)) {
+   echo '<p>';
+   echo $row['stanowisko'];
+   echo ' : ';
+   echo $row['pensja'];
+   echo 'zł</p>';
 }
 
 mysqli_close($con);
